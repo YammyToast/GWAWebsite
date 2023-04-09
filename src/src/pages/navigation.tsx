374ae38617adc21f568ab16db7b0b0bc121eb85a
= <=>
2023-04-10 00:45:51 +0100

    Initial Data Models. Tailwind Theme. Basic Layout.

---
+++ src/src/pages/navigation.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navigation() {
+    
+    return (
+        <div className='md:h-20 border-accent-1 border-b-2 shadow grid grid-flow-col gap-4'>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+            <div>4</div>
+        </div>
+
+    )
+
+}
--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -3,7 +3,22 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'coral': {
+        DEFAULT: '#FF4D6D',
+        hover: '#CC1A3A'
+      },
+      'background': '#1A1A1A',
+      'text': '#f5f5f5',
+      'accent-1': '#333333',
+      'accent-2': '#BDBDBD'
+    },
+    fontFamily: {
+      'maven': ['Maven Pro', 'sans-serif']
+
+    }
   },
   plugins: [],
 } satisfies Config;
