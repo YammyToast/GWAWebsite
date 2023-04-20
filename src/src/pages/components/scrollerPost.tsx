3cd75e06e73c40c1393128b7882cfd9338e10533
YammyToast <=>
2023-04-20 20:40:18 +0100

    Removal of IndexContent for prop-drilling.

--- src/src/pages/components/scrollerPost.tsx
+++ src/src/pages/components/scrollerPost.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
-export default function ScrollerPostItem() {
+import { PostConfig } from '../index'
+
+
+export default function ScrollerPostItem(props: PostConfig) {
     return(
         // <div className="md:w-full md:h-fit p-6 border-accent-1 border border-t-0 first:border">Test</div>
         // <div className="md:w-full md:h-fit bg-accent-1 border-background-alt border mb-2 p-6 shadow-md cursor-pointer duration-200 rounded-xl hover:bg-background">
         //     <div className="font-bold">Title</div>
         // </div>
-        <div className="w-full h-fit p-6 bg-background-alt duration-200 border-2 mb-2 rounded shadow cursor-pointer border-background-alt hover:border-coral">
-            <div className="font-bold">Title</div>
+        <div className="grid grid-cols-2 w-full h-fit p-6 bg-background-alt duration-200 border-2 mb-2 rounded shadow cursor-pointer border-background-alt hover:border-coral">
+            <div className="font-bold">{props.title}</div>
+            <div className="font-normal"></div>
         </div>
     )
 }
--- src/src/pages/index.tsx
+++ src/src/pages/index.tsx
@@ -6,17 +6,32 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import Navigation from './components/navigation'
-import IndexContent from "./components/indexContent";
+import IndexHero from './components/indexHero'
+import IndexScroller from './components/indexScroller'
 
 import Layout from "./layout";
+
+export type PostConfig = {
+  title: string,
+  thumbnailLink: string,
+  id: number,
+  createdAt: Date,
+}
+
 
 const Home: NextPage = () => {
   //const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const { data } = api.posts.getPost.useQuery()
+  console.log("data: ", data);
 
   return (
     <Layout title="Musecal - Browsing">
-      <IndexContent></IndexContent>
+      <div className="flex md:h-fit md:w-full items-center justify-center z-0">
+        <div className="flex md:w-9/12 md:h-fit flex-col">
+          <IndexHero></IndexHero>
+          <IndexScroller></IndexScroller>
+        </div>
+      </div>
     </Layout>
   );
 };
